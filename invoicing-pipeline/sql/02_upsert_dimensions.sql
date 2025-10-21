c3b0eccdad567659fc69140780c9982264e1e2cc
Syed Aftab <aftab@rockvillegroup.com>
2025-10-21 15:13:27 +0500

    Upsert for seller is added

--- invoicing-pipeline/sql/02_upsert_dimensions.sql
+++ invoicing-pipeline/sql/02_upsert_dimensions.sql
@@ -9,3 +9,13 @@ client_tax_id=EXCLUDED.client_tax_id, iban=EXCLUDED.iban, updated_at=EXCLUDED.up
 UPDATE invoicing_olap.etl_watermarks SET last_loaded_ts = NOW() WHERE stream_name='clients';
 
 -- Repeat same pattern for sellers and products
+
+-- Upsert sellers
+WITH w AS (SELECT last_loaded_ts FROM invoicing_olap.etl_watermarks WHERE stream_name='sellers')
+INSERT INTO invoicing_olap.dim_seller (seller_id, seller_name, seller_addr, seller_tax_id, iban, updated_at)
+SELECT s.seller_id, s.seller_name, s.seller_address, s.seller_tax_id, s.iban, s.updated_at
+FROM invoicing.sellers s, w WHERE c.updated_at >= w.last_loaded_ts
+ON CONFLICT (client_id) DO UPDATE SET seller_name=EXCLUDED.seller_name, seller_addr=EXCLUDED.seller_addr,
+seller_tax_id=EXCLUDED.seller_tax_id, iban=EXCLUDED.iban, updated_at=EXCLUDED.updated_at;
+
+UPDATE invoicing_olap.etl_watermarks SET last_loaded_ts = NOW() WHERE stream_name='sellers';
